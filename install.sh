1cf94afc5572c80c840e3e115707a0b6f89fb0d0
Tianyong Tang <tang3w@gmail.com>
2017-12-13 18:07:40 +0800

    Simplify installation script

--- install.sh
+++ install.sh
@@ -1,51 +1,23 @@
 #!/bin/bash
 
-if ! hash brew 2>/dev/null
+if ! command -v brew >/dev/null 2>&1
 then
     echo "brew not found." >&2
     exit 1
 fi
 
-if [ -z "$(brew ls --version ctags)" ] && [ -z "$(brew ls --versions ctags-exuberant)" ]
+if ! command -v ctags >/dev/null 2>&1
 then
     brew install ctags-exuberant
 fi
 
-if [ -z "$(brew ls --versions the_silver_searcher)"  ]
+if ! command -v ag >/dev/null 2>&1
 then
     brew install the_silver_searcher
 fi
 
-abspath() {
-    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
-}
-
-if [ -f ~/.vimrc ]
-then
-    if [ "$(readlink ~/.vimrc)" != "$(abspath ~/.vim/.vimrc)" ]
-    then
-        echo ".vimrc exists." >&2
-        exit 1
-    fi
-else
-    ln -s ~/.vim/.vimrc ~/.vimrc
-fi
-
-pushd ~/.vim > /dev/null
-
 git submodule init
 git submodule update
-
-if [ ! -f .git/hooks/post-merge ]
-then
-    action="#!/bin/bash"
-    action="${action}\nvim +PluginClean! +PluginInstall +qall 2>/dev/null"
-    action="${action}\necho 'Done!'"
-    echo -e ${action} > .git/hooks/post-merge
-    chmod +x .git/hooks/post-merge
-fi
-
-popd > /dev/null
 
 vim +PluginInstall +qall
 
